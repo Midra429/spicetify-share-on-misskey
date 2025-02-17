0bdaa598609b857c4c7b5950e22527bbf167c091
Midra <me@midra.me>
2025-02-17 12:33:50 +0900

    バグ修正

--- src/createPostText.ts
+++ src/createPostText.ts
@@ -17,7 +17,7 @@ export const createPostText = async (
   try {
     switch (uri.type) {
       // 曲
-      case Spicetify.URI.Type.TRACK: {
+      case Spicetify.URI.Type.TRACK:
         const track = await spotifyApi.track(uri)
 
         text += track.name
@@ -28,37 +28,33 @@ export const createPostText = async (
         }
 
         break
-      }
 
       // アルバム
-      case Spicetify.URI.Type.ALBUM: {
+      case Spicetify.URI.Type.ALBUM:
         const album = await spotifyApi.album(uri)
 
         text += album.name
 
         break
-      }
 
       // アーティスト
-      case Spicetify.URI.Type.ARTIST: {
+      case Spicetify.URI.Type.ARTIST:
         const artist = await spotifyApi.artist(uri)
 
         text += artist.name
 
         break
-      }
 
       // ポッドキャスト・番組
-      case Spicetify.URI.Type.SHOW: {
+      case Spicetify.URI.Type.SHOW:
         const show = await spotifyApi.show(uri)
 
         text += show.name
 
         break
-      }
 
       // エピソード
-      case Spicetify.URI.Type.EPISODE: {
+      case Spicetify.URI.Type.EPISODE:
         const episode = await spotifyApi.episode(uri)
 
         text += episode.name
@@ -66,17 +62,17 @@ export const createPostText = async (
         text += episode.show.name
 
         break
-      }
 
       // プレイリスト
+      case Spicetify.URI.Type.PLAYLIST_V2:
+        uri.type = Spicetify.URI.Type.PLAYLIST
+
       case Spicetify.URI.Type.PLAYLIST:
-      case Spicetify.URI.Type.PLAYLIST_V2: {
         const playlist = await spotifyApi.playlist(uri)
 
         text += playlist.name
 
         break
-      }
     }
 
     // ハッシュタグ
